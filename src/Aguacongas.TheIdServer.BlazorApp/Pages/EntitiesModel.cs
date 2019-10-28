dafaa95845e35975b76be72e4a03279755ace36d
Olivier Lefebvre <aguacongas@gmail.com>
2019-10-28 23:42:05 +0100

    refactor: add api page

---
+++ src/Aguacongas.TheIdServer.BlazorApp/Pages/EntitiesModel.cs
@@ -0,0 +1,122 @@
+﻿using Aguacongas.IdentityServer.Store;
+using Aguacongas.TheIdServer.BlazorApp.Services;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Aguacongas.TheIdServer.BlazorApp.Pages
+{
+    public abstract class EntitiesModel<T> : ComponentBase, IDisposable where T: class
+    {
+        private PageRequest _pageRequest;
+        private CancellationTokenSource _cancellationTokenSource;
+
+        [Inject]
+        public IAdminStore<T> AdminStore { get; set; }
+
+        [Inject]
+        public GridState GridState { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        protected IEnumerable<T> EntityList { get; private set; }
+
+        protected string Filter { get; set; }
+
+        protected abstract string SelectProperties { get; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync().ConfigureAwait(false);
+            _pageRequest = new PageRequest
+            {
+                Select = SelectProperties,
+                Take = 10
+            };
+            await GetClientList(_pageRequest)
+                .ConfigureAwait(false);
+
+            GridState.OnHeaderClicked += async e =>
+            {
+                _pageRequest.OrderBy = e.OrderBy;
+                await GetClientList(_pageRequest)
+                    .ConfigureAwait(false);
+                StateHasChanged();
+            };
+        }
+
+        protected async Task OnFilterChanged(string filter)
+        {
+            Filter = filter;
+            var propertyArray = SelectProperties.Split(',');
+            var expressionArray = new string[propertyArray.Length];
+            for(int i = 0; i< propertyArray.Length; i++)
+            {
+                expressionArray[i] = $"contains({propertyArray[i]},'{filter}')";
+            }
+            _pageRequest.Filter = string.Join(" or ", expressionArray);
+
+            _cancellationTokenSource?.Cancel();
+            _cancellationTokenSource = new CancellationTokenSource();
+            var token = _cancellationTokenSource.Token;
+
+            await Task.Delay(500, token)
+                .ContinueWith(async task =>
+                {
+                    if (task.IsCanceled)
+                    {
+                        return;
+                    }
+
+                    var page = await AdminStore.GetAsync(_pageRequest, token)
+                                .ConfigureAwait(false);
+
+                    EntityList = page.Items;
+                    StateHasChanged();
+                },TaskScheduler.Default)
+                .ConfigureAwait(false);
+        }
+
+        [SuppressMessage("Globalization", "CA1304:Specify CultureInfo", Justification = "Url")]
+        protected void OnRowClicked(string entityId)
+        {
+            NavigationManager.NavigateTo($"{typeof(T).Name.ToLower()}/{entityId}");
+        }
+
+        private async Task GetClientList(PageRequest pageRequest)
+        {
+            var page = await AdminStore.GetAsync(pageRequest)
+                            .ConfigureAwait(false);
+            EntityList = page.Items;
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _cancellationTokenSource?.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
--- src/Aguacongas.TheIdServer.BlazorApp/Pages/EntityModel.cs
+++ src/Aguacongas.TheIdServer.BlazorApp/Pages/EntityModel.cs
@@ -1,98 +1,59 @@
 ﻿using Aguacongas.IdentityServer.Store;
-using Aguacongas.TheIdServer.BlazorApp.Services;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Text;
-using System.Threading;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Aguacongas.TheIdServer.BlazorApp.Pages
 {
-    public abstract class EntityModel<T>: ComponentBase where T: class
+    public abstract class EntityModel<T> : ComponentBase where T: class, new()
     {
-        private PageRequest _pageRequest;
-        private CancellationTokenSource _cancellationTokenSource;
-
         [Inject]
         public IAdminStore<T> AdminStore { get; set; }
 
-        [Inject]
-        public GridState GridState { get; set; }
+        [Parameter]
+        public string Id { get; set; }
 
-        [Inject]
-        public NavigationManager NavigationManager { get; set; }
+        protected bool IsNew { get; private set; }
 
-        protected IEnumerable<T> EntityList { get; private set; }
+        protected T Model { get; private set; }
 
-        protected string Filter { get; set; }
-
-        protected abstract string SelectProperties { get; }
+        protected IReadOnlyDictionary<string, object> InputTextAttributes { get; set; }
+            = new Dictionary<string, object>
+            {
+                ["class"] = "form-control"
+            };
+        protected abstract string Expand { get; }
 
         protected override async Task OnInitializedAsync()
         {
-            await base.OnInitializedAsync().ConfigureAwait(false);
-            _pageRequest = new PageRequest
+            if (Id == null)
             {
-                Select = SelectProperties,
-                Take = 10
-            };
-            await GetClientList(_pageRequest)
-                .ConfigureAwait(false);
-
-            GridState.OnHeaderClicked += async e =>
-            {
-                _pageRequest.OrderBy = e.OrderBy;
-                await GetClientList(_pageRequest)
-                    .ConfigureAwait(false);
-                StateHasChanged();
-            };
-        }
-
-        protected async Task OnFilterChanged(string filter)
-        {
-            Filter = filter;
-            var propertyArray = SelectProperties.Split(',');
-            var expressionArray = new string[propertyArray.Length];
-            for(int i = 0; i< propertyArray.Length; i++)
-            {
-                expressionArray[i] = $"contains({propertyArray[i]},'{filter}')";
+                IsNew = true;
+                Model = Create();
+                return;
             }
-            _pageRequest.Filter = string.Join(" or ", expressionArray);
 
-            _cancellationTokenSource?.Cancel();
-            _cancellationTokenSource = new CancellationTokenSource();
-            var token = _cancellationTokenSource.Token;
+            Model = await AdminStore.GetAsync(Id, new GetRequest
+            {
+                Expand = Expand
+            }).ConfigureAwait(false);
+        }
 
-            await Task.Delay(500, token)
-                .ContinueWith(async task =>
-                {
-                    if (task.IsCanceled)
-                    {
-                        return;
-                    }
+        protected async Task HandleValidSubmit()
+        {
+            if (IsNew)
+            {
+                await AdminStore.CreateAsync(Model)
+                    .ConfigureAwait(false);
+                return;
+            }
 
-                    var page = await AdminStore.GetAsync(_pageRequest, token)
-                                .ConfigureAwait(false);
-
-                    EntityList = page.Items;
-                    StateHasChanged();
-                },TaskScheduler.Default)
+            await AdminStore.UpdateAsync(Model)
                 .ConfigureAwait(false);
         }
 
-        [SuppressMessage("Globalization", "CA1304:Specify CultureInfo", Justification = "Url")]
-        protected void OnRowClicked(string entityId)
-        {
-            NavigationManager.NavigateTo($"{typeof(T).Name.ToLower()}/{entityId}");
-        }
-
-        private async Task GetClientList(PageRequest pageRequest)
-        {
-            var page = await AdminStore.GetAsync(pageRequest)
-                            .ConfigureAwait(false);
-            EntityList = page.Items;
-        }
+        protected abstract T Create();
     }
 }
