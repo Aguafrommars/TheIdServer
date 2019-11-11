f9b1fe3606642fedf3f785e7a1931e43d37bf7f0
Olivier Lefebvre <aguacongas@gmail.com>
2019-11-11 21:58:18 +0100

    refactor: add token component

---
+++ src/Aguacongas.TheIdServer.BlazorApp/Models/Token.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aguacongas.TheIdServer.BlazorApp.Models
+{
+    public class Token : INotifyPropertyChanged
+    {
+        public const string RegulatExpression = @"(?<DaysTime>^\d+\s\d\d?:\d\d?:\d\d?$)|(?<Time>^\d\d?:\d\d?:\d\d?$)|(?<MinutesSecondes>^\d\d?:\d\d?$)|(?<Days>^\d+d$)|(?<Hours>^\d+h$)|(?<Minutes>^\d+m$)|(?<Secondes>^\d+s?$)";
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _valueString;
+        [RegularExpression(RegulatExpression, ErrorMessage = "The token expression doesn't math a valid faormat")]
+        public string ValueString 
+        {
+            get { return _valueString; }
+            set
+            {
+                _valueString = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ValueString)));
+            }
+        }
+    }
+}
