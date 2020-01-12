ee25ecb460945df2c953a9ced805e500ab4e107f
Olivier Lefebvre <aguacongas@gmail.com>
2020-01-12 22:31:52 +0100

    refactor: remove code smell

--- src/Aguacongas.TheIdServer.BlazorApp/Components/UserComponents/UserRoles.razor.cs
+++ src/Aguacongas.TheIdServer.BlazorApp/Components/UserComponents/UserRoles.razor.cs
@@ -1,7 +1,5 @@
-﻿using Aguacongas.TheIdServer.BlazorApp.Models;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using System.Collections.Generic;
-using System.Linq;
 using Entity = Aguacongas.IdentityServer.Store.Entity;
 
 namespace Aguacongas.TheIdServer.BlazorApp.Components.UserComponents
@@ -16,20 +14,5 @@ namespace Aguacongas.TheIdServer.BlazorApp.Components.UserComponents
 
         [Parameter]
         public EventCallback<Entity.Role> RoleValueChanged { get; set; }
-
-        private bool Validate(string role)
-        {
-            if (Model.Any(c => c.Name == role))
-            {
-                _notifier.Notify(new Notification
-                {
-                    Header = "Invalid role",
-                    IsError = true,
-                    Message = $"This user already contains the role {role}"
-                });
-                return false;
-            }
-            return true;
-        }
     }
 }
