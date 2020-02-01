ce34d834a55ecffc18d937eed8754226c05463a0
Olivier Lefebvre <aguacongas@gmail.com>
2020-02-01 16:40:57 +0100

    refactor: remove using

--- src/Aguacongas.TheIdServer.BlazorApp/Models/Token.cs
+++ src/Aguacongas.TheIdServer.BlazorApp/Models/Token.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
 
 namespace Aguacongas.TheIdServer.BlazorApp.Models
 {
@@ -9,7 +8,6 @@ namespace Aguacongas.TheIdServer.BlazorApp.Models
         public event PropertyChangedEventHandler PropertyChanged;
 
         private string _valueString;
-        [RegularExpression(RegulatExpression, ErrorMessage = "The token expression doesn't match a valid format. You can use the forms d.hh:mm:ss, hh.mm:ss, mm:ss, a number of days (365d), a number of hours (12h), a number of minutes (30m), a number of second")]
         public string ValueString 
         {
             get { return _valueString; }
