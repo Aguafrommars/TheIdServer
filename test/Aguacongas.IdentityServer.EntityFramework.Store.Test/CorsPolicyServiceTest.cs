568491aeb47b71f7e48f43b4f1988ced48deecc6
Olivier Lefebvre <aguacongas@gmail.com>
2020-02-08 18:33:21 +0100

    test: add more

---
+++ test/Aguacongas.IdentityServer.EntityFramework.Store.Test/CorsPolicyServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using IdentityServer4.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Aguacongas.IdentityServer.EntityFramework.Store.Test
+{
+    public class CorsPolicyServiceTest
+    {
+        [Fact]
+        public void Constructor_should_validate_parameters()
+        {
+            Assert.Throws<ArgumentNullException>(() => new CorsPolicyService(null));
+        }
+
+        [Fact]
+        public async Task IsOriginAllowedAsync_should_return_false()
+        {
+            var provider = new ServiceCollection()
+                .AddLogging()
+                .AddConfigurationEntityFrameworkStores(options =>
+                    options.UseInMemoryDatabase(Guid.NewGuid().ToString()))
+                .BuildServiceProvider();
+
+            var sut = provider.GetRequiredService<ICorsPolicyService>();
+
+            Assert.False(await sut.IsOriginAllowedAsync("http://test"));
+        }
+    }
+}
