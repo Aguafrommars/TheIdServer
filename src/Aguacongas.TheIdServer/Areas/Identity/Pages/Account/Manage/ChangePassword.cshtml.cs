8041bfbb457a35ec59b09054a3f5d037fd07916d
Olivier Lefebvre <aguacongas@gmail.com>
2020-03-15 23:33:32 +0100

    ci: update blazor version

--- src/Aguacongas.TheIdServer/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
+++ src/Aguacongas.TheIdServer/Areas/Identity/Pages/Account/Manage/ChangePassword.cshtml.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
-using Aguacongas.TheIdServer.Models;
+﻿using Aguacongas.TheIdServer.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 namespace Aguacongas.TheIdServer.Areas.Identity.Pages.Account.Manage
 {
     public class ChangePasswordModel : PageModel
--- src/Aguacongas.TheIdServer/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ src/Aguacongas.TheIdServer/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -1,16 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Text.Encodings.Web;
-using System.Threading.Tasks;
-using Aguacongas.TheIdServer.Models;
+﻿using Aguacongas.TheIdServer.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
 
 namespace Aguacongas.TheIdServer.Areas.Identity.Pages.Account.Manage
 {
