3f5395df180ff5433d64f18dc84ffd8998465c1a
Olivier Lefebvre <aguacongas@gmail.com>
2020-03-16 20:03:55 +0100

    refactor: remove unused project

--- src/Blazor/Aguacongas.Blazor.Oidc/SerializableClaim.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Aguacongas.TheIdServer.Blazor.Oidc
-{
-    public class SerializableClaim
-    {
-        public string Type { get; set; }
-        public string Value { get; set; }
-    }
-}
