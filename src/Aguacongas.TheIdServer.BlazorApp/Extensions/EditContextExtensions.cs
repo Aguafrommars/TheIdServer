a5b319ab9f61a16d297d76fbcfe47efff3cabc99
Olivier Lefebvre <aguacongas@gmail.com>
2020-03-29 21:11:07 +0200

    feat: custom welcome screen

--- src/Aguacongas.TheIdServer.BlazorApp/Extensions/EditContextExtensions.cs
+++ src/Aguacongas.TheIdServer.BlazorApp/Extensions/EditContextExtensions.cs
@@ -7,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Models = Aguacongas.TheIdServer.BlazorApp.Models;
 
 namespace Microsoft.AspNetCore.Components.Forms
 {
