4ebf6c878905344fcdfb0cb4cd161e622b3e7e76
Olivier Lefebvre <aguacongas@gmail.com>
2020-05-24 16:08:41 +0200

    ci: :bug: version number

--- appveyorinit.ps1
+++ appveyorinit.ps1
@@ -19,10 +19,10 @@ appveyor AddMessage "SemVer = $nextversion"
 
 if (![string]::IsNullOrEmpty($env:GitVersion_PreReleaseLabel))
 {
-    $preReleaseLabel = $env:GitVersion_PreReleaseTagWithDash.Trim()
+    $preReleaseLabel = $env:GitVersion_PreReleaseLabel.Trim()
     if (![string]::IsNullOrEmpty($preReleaseLabel))
     {
-        $nextversion = "$nextversion$preReleaseLabel.$env:GitVersion_CommitsSinceVersionSourcePadded"
+        $nextversion = "$nextversion-$preReleaseLabel$env:GitVersion_PreReleaseNumber-$env:GitVersion_CommitsSinceVersionSourcePadded"
     }
 }
 $nextversion = $nextversion.Trim()
