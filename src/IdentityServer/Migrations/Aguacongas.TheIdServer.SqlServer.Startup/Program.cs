f32ceb0995a8d9217b85fa030bfa483d64448e11
Olivier Lefebvre <aguacongas@gmail.com>
2020-07-24 23:00:55 +0200

    feat: multi RDBMS migrations

---
+++ src/IdentityServer/Migrations/Aguacongas.TheIdServer.SqlServer.Startup/Program.cs
@@ -0,0 +1,35 @@
+﻿using Aguacongas.TheIdServer.Data;
+using Aguacongas.TheIdServer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Aguacongas.TheIdServer.SqlServer
+{
+    [SuppressMessage("Major Code Smell", "S1118:Utility classes should not have public constructors", Justification = "<Pending>")]
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(builder => builder.AddJsonFile("appsettings.json"))
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var cn = hostContext.Configuration.GetConnectionString("db");
+
+                    Action<DbContextOptionsBuilder> optionsAction = options => options.UseSqlServer(cn, options => options.MigrationsAssembly("Aguacongas.TheIdServer.SqlServer"));
+                    services.AddDbContext<ApplicationDbContext>(optionsAction)
+                        .AddIdentityServer4AdminEntityFrameworkStores<ApplicationUser, ApplicationDbContext>()
+                        .AddConfigurationEntityFrameworkStores(optionsAction)
+                        .AddOperationalEntityFrameworkStores(optionsAction);
+                });
+
+    }
+}
