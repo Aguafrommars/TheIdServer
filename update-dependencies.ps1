8647caf2900d3654509bb2debc6af96b7fc33c58
Olivier Lefebvre <aguacongas@gmail.com>
2020-09-11 17:12:18 +0200

    Update update-dependencies.ps1

--- update-dependencies.ps1
+++ update-dependencies.ps1
@@ -68,8 +68,8 @@ Write-Host "git commit -m ""fix: update packages"""
 git commit -m "fix: update packages"
 Write-Host "git brang $branchName"
 git brang $branchName
-Write-Host "git checkout $branchName"
-git checkout $branchName
+Write-Host "git switch $branchName"
+git switch $branchName
 Write-Host "git push -u origin $branchName"
 git push -u origin $branchName
 
