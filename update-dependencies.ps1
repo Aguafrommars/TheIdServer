ea670b12740d7d424782c2522f7960a50a3590f9
Olivier Lefebvre <aguacongas@gmail.com>
2020-09-11 09:57:17 +0200

    Update update-dependencies.ps1

--- update-dependencies.ps1
+++ update-dependencies.ps1
@@ -61,8 +61,10 @@ Write-Host "git add ."
 git add .
 Write-Host "git commit -m ""fix: update packages"""
 git commit -m "fix: update packages"
-Write-Host "git push --set-upstream origin $branchName"
-git push --set-upstream origin $branchName
+Write-Host "git checkout -b $branchName"
+git checkout -b $branchName
+Write-Host "git push -u origin HEAD"
+git push -u origin HEAD
 
 $authorization = "Bearer $env:GITHUB_TOKEN"
 $createPrUrl = 'https://api.github.com/repos/Aguafrommars/TheIdServer/pulls'
