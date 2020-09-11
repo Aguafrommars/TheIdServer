51bc798c9674742651bf0e7d2e53fb553ffd9473
Olivier Lefebvre <aguacongas@gmail.com>
2020-09-11 08:36:15 +0200

    Update update-dependencies.ps1
    Add log

--- update-dependencies.ps1
+++ update-dependencies.ps1
@@ -6,21 +6,22 @@
     $return = $false
     $packageLineList = dotnet list $project package --outdated
     foreach($line in $packageLineList) {
+       Write-Host $line
+       
        $match = $line -match '>\s(\S*)\s*\S*\s*\S*\s*(\S*)'
        if (!$match) {
           continue
        }
+       
+       $added = dotnet add $project package $Matches.1 --version $Matches.2
 
-        Write-Host $line
-        $added = dotnet add $project package $Matches.1 --version $Matches.2
+       if ($LASTEXITCODE -ne 0) {
+           Write-Error "dotnet add $project package $Matches.1 --version $Matches.2 exit with code $LASTEXITCODE"
+           Write-Host $added
+           break
+       }
 
-        if ($LASTEXITCODE -ne 0) {
-            Write-Error "dotnet add $project package $Matches.1 --version $Matches.2 exit with code $LASTEXITCODE"
-            Write-Host $added
-            break
-        }
-
-        $return = $true
+       $return = $true
     }
 
     return $return
@@ -51,11 +52,16 @@ if (!$updated) {
 
 $branchName = "fix/update-dependencies"
 
+Write-Host "git add ."
 git add .
+Write-Host "git commit -m ""fix: update packages"""
 git commit -m "fix: update packages"
+Write-Host "git branch $branchName"
 git branch $branchName
+Write-Host "git checkout $branchName"
 git checkout $branchName
 git push
+Write-Host "git push"
 
 $authorization = "Bearer $env:GITHUB_TOKEN"
 $createPrUrl = 'https://api.github.com/repos/Aguafrommars/TheIdServer/pulls'
@@ -64,4 +70,5 @@ $headers = @{
     Accept = "application/vnd.github.v3+json"
 }
 $payload = "{ ""title"": ""update packages"", ""head"": ""$branchName"", ""base"": ""master"" }"
+Write-Host "Invoke-WebRequest -Uri $createPrUrl -Headers $headers -Method ""POST"" -Body $payload"
 Invoke-WebRequest -Uri $createPrUrl -Headers $headers -Method "POST" -Body $payload
