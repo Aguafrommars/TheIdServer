2ce2bbe4e44d5ac97ecf2f5f70f6505b390bfe65
Olivier Lefebvre <olivier.lefebvre@live.com>
2020-09-30 19:44:22 +0200

    test: test CertificateXmlEncryptor

---
+++ test/Aguacongas.IdentityServer.KeysRotation.Test/XmlEncryption/CertificateXmlEncryptorTest.cs
@@ -0,0 +1,17 @@
+﻿using Aguacongas.IdentityServer.KeysRotation.XmlEncryption;
+using System;
+using Xunit;
+
+namespace Aguacongas.IdentityServer.KeysRotation.Test.XmlEncryption
+{
+    public class CertificateXmlEncryptorTest
+    {
+        [Fact]
+        public void Constructor_should_throw_on_args_null()
+        {
+            Assert.Throws<ArgumentNullException>(() => new CertificateXmlEncryptor(null, null));
+            Assert.Throws<ArgumentNullException>(() => new CertificateXmlEncryptor(null, null, null));
+            Assert.Throws<ArgumentNullException>(() => new CertificateXmlEncryptor("test", null, null));
+        }
+    }
+}
