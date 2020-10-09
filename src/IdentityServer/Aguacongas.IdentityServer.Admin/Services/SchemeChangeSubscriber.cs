626e89eed153dfbf51c6355501a5ff937d057640
Olivier Lefebvre <olivier.lefebvre@live.com>
2020-10-09 17:30:07 +0200

    refactor: add signalr client log

--- src/IdentityServer/Aguacongas.IdentityServer.Admin/Services/SchemeChangeSubscriber.cs
+++ src/IdentityServer/Aguacongas.IdentityServer.Admin/Services/SchemeChangeSubscriber.cs
@@ -5,6 +5,7 @@ using Aguacongas.IdentityServer.Abstractions;
 using Aguacongas.IdentityServer.KeysRotation;
 using Microsoft.AspNetCore.DataProtection.AuthenticatedEncryption.ConfigurationModel;
 using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,6 +22,7 @@ namespace Aguacongas.IdentityServer.Admin.Services
         private readonly IDynamicProviderStore<TSchemeDefinition> _store;
         private readonly KeyManagerWrapper<IAuthenticatedEncryptorDescriptor> _dataProtectionKeyManagerWrapper;
         private readonly KeyManagerWrapper<RsaEncryptorDescriptor> _signingKeyManagerWrapper;
+        private readonly ILogger<SchemeChangeSubscriber<TSchemeDefinition>> _logger;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="SchemeChangeSubscriber{TSchemeDefinition}" /> class.
@@ -30,6 +32,7 @@ namespace Aguacongas.IdentityServer.Admin.Services
         /// <param name="store">The store.</param>
         /// <param name="dataProtectionKeyManagerWrapper">The data protection key manager wrapper.</param>
         /// <param name="signingKeyManagerWrapper">The signing key manager wrapper.</param>
+        /// <param name="logger">The logger.</param>
         /// <exception cref="ArgumentNullException">factory
         /// or
         /// manager
@@ -39,13 +42,15 @@ namespace Aguacongas.IdentityServer.Admin.Services
             NoPersistentDynamicManager<TSchemeDefinition> manager,
             IDynamicProviderStore<TSchemeDefinition> store,
             KeyManagerWrapper<IAuthenticatedEncryptorDescriptor> dataProtectionKeyManagerWrapper,
-            KeyManagerWrapper<RsaEncryptorDescriptor> signingKeyManagerWrapper)
+            KeyManagerWrapper<RsaEncryptorDescriptor> signingKeyManagerWrapper,
+            ILogger<SchemeChangeSubscriber<TSchemeDefinition>> logger)
         {
             _factory = factory ?? throw new ArgumentNullException(nameof(factory));
             _manager = manager ?? throw new ArgumentNullException(nameof(manager));
             _store = store ?? throw new ArgumentNullException(nameof(manager));
             _dataProtectionKeyManagerWrapper = dataProtectionKeyManagerWrapper ?? throw new ArgumentNullException(nameof(dataProtectionKeyManagerWrapper));
             _signingKeyManagerWrapper = signingKeyManagerWrapper ?? throw new ArgumentNullException(nameof(signingKeyManagerWrapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         /// <summary>
@@ -61,23 +66,27 @@ namespace Aguacongas.IdentityServer.Admin.Services
 
             connection.On<string>(nameof(IProviderHub.ProviderAdded), async scheme =>
             {
+                _logger.LogInformation($"SignalR notification received: {nameof(IProviderHub.ProviderAdded)}({scheme})");
                 var definition = await _store.FindBySchemeAsync(scheme).ConfigureAwait(false);
                 await _manager.AddAsync(definition).ConfigureAwait(false);
             });
 
             connection.On<string>(nameof(IProviderHub.ProviderRemoved), async scheme =>
             {
+                _logger.LogInformation($"SignalR notification received: {nameof(IProviderHub.ProviderRemoved)}({scheme})");
                 await _manager.RemoveAsync(scheme).ConfigureAwait(false);
             });
 
             connection.On<string>(nameof(IProviderHub.ProviderUpdated), async scheme =>
             {
+                _logger.LogInformation($"SignalR notification received: {nameof(IProviderHub.ProviderUpdated)}({scheme})");
                 var definition = await _store.FindBySchemeAsync(scheme).ConfigureAwait(false);
                 await _manager.UpdateAsync(definition).ConfigureAwait(false);
             });
 
             connection.On<string, string>(nameof(IProviderHub.KeyRevoked), (kind, id) =>
             {
+                _logger.LogInformation($"SignalR notification received: {nameof(IProviderHub.KeyRevoked)}({kind}, {id})");
                 var keyId = Guid.Parse(id);
                 if (kind == nameof(IAuthenticatedEncryptorDescriptor))
                 {
