49d0d5d311aaa17e66d2c6895dee0ba75ae931cf
Olivier Lefebvre <olivier.lefebvre@live.com>
2020-10-26 16:28:05 +0100

    refactor: lazy load sub pages

--- src/Aguacongas.TheIdServer.BlazorApp/App.razor.cs
+++ src/Aguacongas.TheIdServer.BlazorApp/App.razor.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Components.Routing;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -9,22 +8,45 @@ namespace Aguacongas.TheIdServer.BlazorApp
 {
     public partial class App
     {
-        private List<Assembly> _lazyLoadedAssemblies = new List<Assembly>
+        private readonly List<Assembly> _lazyLoadedAssemblies = new List<Assembly>
         {
             typeof(Pages.Index).Assembly
         };
 
+        private readonly string[] _pageKindList = new []
+        {
+            "Api",
+            "ApiScope",
+            "Client",
+            "ExternalProvider",
+            "Identities",
+            "Identity",
+            "Import",
+            "Key",
+            "Role",
+            "User"
+        };
+
         private Task OnNavigateAsync(NavigationContext args)
         {
-            if (args.Path.StartsWith("apis"))
-            {
-                return LoadAssemblyAsync("Aguacongas.TheIdServer.BlazorApp.Pages.Apis.dll");
-            }
-
-            if (args.Path.StartsWith("protectresource"))
+            var path = args.Path;
+            if (path.StartsWith("protectresource"))
             {
                 return LoadAssemblyAsync("Aguacongas.TheIdServer.BlazorApp.Pages.Api.dll");
             }
+
+            var pageKind = _pageKindList.FirstOrDefault(k => path.StartsWith($"{k.ToLower()}s"));
+            if (pageKind != null)
+            {
+                return LoadAssemblyAsync($"Aguacongas.TheIdServer.BlazorApp.Pages.{pageKind}s.dll");
+            }
+
+            pageKind = _pageKindList.FirstOrDefault(k => path.StartsWith($"{k.ToLower()}"));
+            if (pageKind != null)
+            {
+                return LoadAssemblyAsync($"Aguacongas.TheIdServer.BlazorApp.Pages.{pageKind}.dll");
+            }
+
             return Task.CompletedTask;
         }
 
