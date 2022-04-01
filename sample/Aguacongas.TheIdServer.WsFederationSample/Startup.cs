cb35fb7ba2945810901ad0ccf982a0f2b8fe198a
Olivier Lefebvre <aguacongas@gmail.com>
2022-04-01 21:53:31 +0200

    doc: fix ws-fed metadat url

--- sample/Aguacongas.TheIdServer.WsFederationSample/Startup.cs
+++ sample/Aguacongas.TheIdServer.WsFederationSample/Startup.cs
@@ -27,7 +27,7 @@ namespace Aguacongas.TheIdServer.WsFederationSample
                 })
                 .AddWsFederation(options =>
                 {
-                    options.MetadataAddress = "https://localhost:5001/wsfed";
+                    options.MetadataAddress = "https://localhost:5001/wsfederation/metadata";
                     options.RequireHttpsMetadata = false;
 
                     options.Wtrealm = "urn:aspnetcorerp";
