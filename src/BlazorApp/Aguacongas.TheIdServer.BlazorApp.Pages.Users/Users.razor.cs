4bb64123a519af841893a1aabb43317da56a10f7
Eversong Woods <eversong7521@hotmail.com>
2024-10-15 14:36:36 +0000

    Fixed Users page pagination issues

--- src/BlazorApp/Aguacongas.TheIdServer.BlazorApp.Pages.Users/Users.razor.cs
+++ src/BlazorApp/Aguacongas.TheIdServer.BlazorApp.Pages.Users/Users.razor.cs
@@ -14,24 +14,5 @@ namespace Aguacongas.TheIdServer.BlazorApp.Pages.Users
         protected override string SelectProperties => $"{nameof(Entity.User.Id)},{nameof(Entity.User.UserName)}";
 
         protected override string ExportExpand => $"{nameof(Entity.User.UserClaims)},{nameof(Entity.User.UserRoles)}";
-
-        [Parameter]
-        public int PageSize { get; set; } = 10;
-
-        [Parameter]
-        public int CurrentPage { get; set; } = 1;
-
-        public int TotalPages => (int)(EntityList?.Count() / (double)PageSize) + 1;
-
-        protected IEnumerable<Entity.User> PagedEntityList => EntityList?.Skip((CurrentPage - 1) * PageSize).Take(PageSize);
-
-        protected void NavigateToPage(int page)
-        {
-            if (page < 1 || page > TotalPages)
-                return;
-
-            CurrentPage = page;
-            StateHasChanged();
-        }
     }
 }
